2057d11468b025997031407eaae410e15b2c8095
unknown <oyurt43@gmail.com>
2023-02-08 12:20:02 +0300

    tablestorage

---
+++ MvcWebApp/Controllers/TableStoragesController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using AzureStorageLibrary;
+using AzureStorageLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace MvcWebApp.Controllers
+{
+    public class TableStoragesController : Controller
+    {
+        private readonly INoSqlStorage<Product> _noSqlStorage;
+
+        public TableStoragesController(INoSqlStorage<Product> noSqlStorage)
+        {
+            _noSqlStorage = noSqlStorage;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.products = _noSqlStorage.All().ToList();
+            ViewBag.IsUpdate = false;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            product.RowKey = Guid.NewGuid().ToString();
+            product.PartitionKey = "Kalemler";
+
+            await _noSqlStorage.Add(product);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Update(string rowKey, string partitionKey)
+        {
+            var product = await _noSqlStorage.Get(rowKey, partitionKey);
+
+            ViewBag.products = _noSqlStorage.All().ToList();
+            ViewBag.IsUpdate = true;
+
+            return View("Index", product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Product product)
+        {
+            ViewBag.IsUpdate = true;
+
+            await _noSqlStorage.Update(product);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(string rowKey,string partitionKey)
+        {
+            await _noSqlStorage.Delete(rowKey, partitionKey);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Query(int price)
+        {
+            ViewBag.IsUpdate = false;
+            ViewBag.products = _noSqlStorage.Query(x=>x.Price>price).ToList();
+            return View("Index");
+        }
+    }
+}
